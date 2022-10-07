9315754964235e3ec1bf91fd6a448a48976e7ec8
Matilda Mared <matildamared@live.se>
2022-10-07 08:41:55 +0200

    add animations for hamburger menu

--- src/components/MobileMenu/MobileMenu.js
+++ src/components/MobileMenu/MobileMenu.js
@@ -1,101 +1,136 @@
 /* eslint-disable no-unused-vars */
-import React from 'react';
-import styled from 'styled-components/macro';
-import { DialogOverlay, DialogContent } from '@reach/dialog';
+import React from "react";
+import styled, { keyframes } from "styled-components/macro";
+import { DialogOverlay, DialogContent } from "@reach/dialog";
 
-import { QUERIES, WEIGHTS } from '../../constants';
+import { QUERIES, WEIGHTS } from "../../constants";
 
-import UnstyledButton from '../UnstyledButton';
-import Icon from '../Icon';
-import VisuallyHidden from '../VisuallyHidden';
+import UnstyledButton from "../UnstyledButton";
+import Icon from "../Icon";
+import VisuallyHidden from "../VisuallyHidden";
 
 const MobileMenu = ({ isOpen, onDismiss }) => {
-  return (
-    <Overlay isOpen={isOpen} onDismiss={onDismiss}>
-      <Content aria-label="Menu">
-        <CloseButton onClick={onDismiss}>
-          <Icon id="close" />
-          <VisuallyHidden>Dismiss menu</VisuallyHidden>
-        </CloseButton>
-        <Filler />
-        <Nav>
-          <NavLink href="/sale">Sale</NavLink>
-          <NavLink href="/new">New&nbsp;Releases</NavLink>
-          <NavLink href="/men">Men</NavLink>
-          <NavLink href="/women">Women</NavLink>
-          <NavLink href="/kids">Kids</NavLink>
-          <NavLink href="/collections">Collections</NavLink>
-        </Nav>
-        <Footer>
-          <SubLink href="/terms">Terms and Conditions</SubLink>
-          <SubLink href="/privacy">Privacy Policy</SubLink>
-          <SubLink href="/contact">Contact Us</SubLink>
-        </Footer>
-      </Content>
-    </Overlay>
-  );
+	return (
+		<Overlay isOpen={isOpen} onDismiss={onDismiss}>
+			<Content aria-label="Menu">
+				<CloseButton onClick={onDismiss}>
+					<Icon id="close" />
+					<VisuallyHidden>Dismiss menu</VisuallyHidden>
+				</CloseButton>
+				<Filler />
+				<Nav>
+					<NavLink href="/sale">Sale</NavLink>
+					<NavLink href="/new">New&nbsp;Releases</NavLink>
+					<NavLink href="/men">Men</NavLink>
+					<NavLink href="/women">Women</NavLink>
+					<NavLink href="/kids">Kids</NavLink>
+					<NavLink href="/collections">Collections</NavLink>
+				</Nav>
+				<Footer>
+					<SubLink href="/terms">Terms and Conditions</SubLink>
+					<SubLink href="/privacy">Privacy Policy</SubLink>
+					<SubLink href="/contact">Contact Us</SubLink>
+				</Footer>
+			</Content>
+		</Overlay>
+	);
 };
 
-const Overlay = styled(DialogOverlay)`
-  position: fixed;
-  top: 0;
-  left: 0;
-  right: 0;
-  bottom: 0;
-  background: var(--color-backdrop);
-  display: flex;
-  justify-content: flex-end;
-`;
-
-const Content = styled(DialogContent)`
-  background: white;
-  width: 300px;
-  height: 100%;
-  padding: 24px 32px;
-  display: flex;
-  flex-direction: column;
-`;
-
-const CloseButton = styled(UnstyledButton)`
-  position: absolute;
-  top: 10px;
-  right: 0;
-  padding: 16px;
-`;
-
-const Nav = styled.nav`
-  display: flex;
-  flex-direction: column;
-  gap: 16px;
-`;
-
-const NavLink = styled.a`
-  color: var(--color-gray-900);
-  font-weight: ${WEIGHTS.medium};
-  text-decoration: none;
-  font-size: 1.125rem;
-  text-transform: uppercase;
-
-  &:first-of-type {
-    color: var(--color-secondary);
+const fadeIn = keyframes`
+  0% {
+    opacity: 0;
+  }
+  100% {
+    opacity: 1;
   }
 `;
 
+const slideInFromRight = keyframes`
+  0% {
+    transform: translateX(100%);
+  }
+  100% {
+    transform: translateX(0)
+  }
+`;
+
+const Overlay = styled(DialogOverlay)`
+	position: fixed;
+	top: 0;
+	left: 0;
+	right: 0;
+	bottom: 0;
+	background: var(--color-backdrop);
+	display: flex;
+	justify-content: flex-end;
+	animation: ${fadeIn} 1000ms;
+`;
+
+const Content = styled(DialogContent)`
+	background: white;
+	width: 300px;
+	height: 100%;
+	padding: 24px 32px;
+	display: flex;
+	flex-direction: column;
+	animation: ${slideInFromRight} 500ms;
+	animation-delay: 400ms;
+	transform: translateX(100%);
+	animation-fill-mode: forwards;
+`;
+
+const CloseButton = styled(UnstyledButton)`
+	position: absolute;
+	top: 10px;
+	right: 0;
+	padding: 16px;
+	animation: ${fadeIn} 500ms;
+	opacity: 0;
+	animation-fill-mode: forwards;
+	animation-delay: 1000ms;
+`;
+
+const Nav = styled.nav`
+	display: flex;
+	flex-direction: column;
+	gap: 16px;
+	animation: ${fadeIn} 500ms;
+	opacity: 0;
+	animation-fill-mode: forwards;
+	animation-delay: 1200ms;
+`;
+
+const NavLink = styled.a`
+	color: var(--color-gray-900);
+	font-weight: ${WEIGHTS.medium};
+	text-decoration: none;
+	font-size: 1.125rem;
+	text-transform: uppercase;
+
+	&:first-of-type {
+		color: var(--color-secondary);
+	}
+`;
+
 const Filler = styled.div`
-  flex: 1;
+	flex: 1;
 `;
 const Footer = styled.footer`
-  flex: 1;
-  display: flex;
-  flex-direction: column;
-  gap: 14px;
-  justify-content: flex-end;
+	flex: 1;
+	display: flex;
+	flex-direction: column;
+	gap: 14px;
+	justify-content: flex-end;
+	animation: ${fadeIn} 500ms;
+	opacity: 0;
+	animation-fill-mode: forwards;
+	animation-delay: 1400ms;
 `;
 
 const SubLink = styled.a`
-  color: var(--color-gray-700);
-  font-size: 0.875rem;
-  text-decoration: none;
+	color: var(--color-gray-700);
+	font-size: 0.875rem;
+	text-decoration: none;
 `;
 
 export default MobileMenu;
